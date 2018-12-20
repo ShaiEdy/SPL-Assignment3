9503bd544642e53b98e0bf5012da690e255bc1d6
עדי <agotliber@gmail..com>
2018-12-20 18:24:10 +0200

    lkj

--- src/main/java/bgu/spl/net/api/bidi/ConnectionsImpl.java
+++ src/main/java/bgu/spl/net/api/bidi/ConnectionsImpl.java
@@ -2,6 +2,8 @@ package bgu.spl.net.api.bidi;
 
 import bgu.spl.net.srv.BlockingConnectionHandler;
 
+import java.util.Set;
+import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionsImpl <T> implements Connections<T> {
@@ -12,20 +14,26 @@ public class ConnectionsImpl <T> implements Connections<T> {
     }
 
     @Override
-    public boolean send(int connectionId, Object msg) {
-        BlockingConnectionHandler CH= idToConnectionHandler.get(connectionId);
-        CH.send(msg); //todo- implement send of BlockingConnectionHandler
-
+    //send the msg to the CH with the given id
+    public boolean send(int connectionId, T msg) {
+        BlockingConnectionHandler blockingCH= idToConnectionHandler.get(connectionId);
+        blockingCH.send(msg);
         return false; //todo- think how to know if the send succeed or not
     }
 
     @Override
-    public void broadcast(Object msg) {
-
+    //send the broadcast to whoever registered
+    public void broadcast(T msg) {
+        Set<Integer> idSet=idToConnectionHandler.keySet();
+        for (Integer integer: idSet){
+            BlockingConnectionHandler blockingCH= idToConnectionHandler.get(integer);
+            blockingCH.send(msg);
+        }
     }
 
     @Override
+    //remove the CH with the given id from the hashMap
     public void disconnect(int connectionId) {
-
+        idToConnectionHandler.remove(connectionId);
     }
 }
