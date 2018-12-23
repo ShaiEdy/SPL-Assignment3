3b2b743a3ac76d8dc15f75fad59a3b9093d6cba9
Shai <shaiadi1@gmail.com>
2018-12-23 18:06:22 +0200

    Hey

--- src/main/java/bgu/spl/net/impl/messages/RegisterMessage.java
+++ src/main/java/bgu/spl/net/impl/messages/RegisterMessage.java
@@ -1,6 +1,16 @@
 package bgu.spl.net.impl.messages;
 
 public class RegisterMessage<T> implements Message<T> {
+    int arrayLength;
+
+    public RegisterMessage(byte[] messageBytesArray) {
+        this.arrayLength = messageBytesArray.length;
+
+        for (int i = 2; i < arrayLength; i++) {
+
+        }
+    }
+
     @Override
     public T act(T message) {
         return null;
