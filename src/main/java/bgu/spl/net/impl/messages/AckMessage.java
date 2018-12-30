c4466736719b2d55b5655d8606d3af44ec2a400d
עדי <agotliber@gmail..com>
2018-12-30 15:22:42 +0200

    lkj

--- src/main/java/bgu/spl/net/impl/messages/AckMessage.java
+++ src/main/java/bgu/spl/net/impl/messages/AckMessage.java
@@ -1,10 +1,14 @@
 package bgu.spl.net.impl.messages;
 
-public class AckMessage<T> extends Message<T>{
+import bgu.spl.net.api.Customer;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class AckMessage extends Message{
     short messageOpcode;
     byte[] optionalBytesArray;
 
-    public AckMessage(byte [] messageByteArray) {
+    public AckMessage(byte [] messageByteArray) { //todo- decide if needed
         byte[] byteArrMsgOpcose= {messageByteArray[2], messageByteArray[3]};
         messageOpcode= bytesToShort(byteArrMsgOpcose);
         int index= 4;
@@ -15,15 +19,21 @@ public class AckMessage<T> extends Message<T>{
         }
     }
 
-    @Override
-    public T act(T message) {
-        return null;
+    public AckMessage(short messageOpcode, byte[]optionalBytesArray ) {
+        this.messageOpcode= messageOpcode;
+        this.optionalBytesArray= optionalBytesArray;
     }
+
 
     public short bytesToShort(byte[] byteArr) {
         short result = (short) ((byteArr[0] & 0xff) << 8);
         result += (short) (byteArr[1] & 0xff);
         return result;
+    }
+
+    @Override
+    protected Message act(ConcurrentHashMap<String, Customer> dataMap) {
+        return null;
     }
 }
 
