74cf39708c9baa4b483780d479058e17116f6d2e
Shai <shaiadi1@gmail.com>
2019-01-01 23:20:58 +0200

    Hey

--- src/main/java/bgu/spl/net/impl/messages/LogOutMessage.java
+++ src/main/java/bgu/spl/net/impl/messages/LogOutMessage.java
@@ -11,7 +11,7 @@ public class LogOutMessage extends Message {
 
     @Override
     public Message act(DataBase dataBase, Customer customer) {
-        if (customer == null || !customer.isLoggedIn()) //if customer is not registered or not logged in
+        if (!customer.isLoggedIn()) //if customer is not registered or not logged in
             return new ErrorMessage((short) 3);
         else return new AckMessage((short) 3, null); //customer is registered and logged in.
     }
--- src/main/java/bgu/spl/net/impl/messages/UserListMessage.java
+++ src/main/java/bgu/spl/net/impl/messages/UserListMessage.java
@@ -2,27 +2,24 @@ package bgu.spl.net.impl.messages;
 
 import bgu.spl.net.api.Customer;
 import bgu.spl.net.api.DataBase;
-
-import java.util.List;
 import java.util.Set;
-import java.util.Vector;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class UserListMessage extends Message {
 
-    public UserListMessage(byte[] bytes) {} //nothing special to build
+    public UserListMessage(byte[] bytes) {
+    } //nothing special to build
 
     @Override
     public Message act(DataBase dataBase, Customer customer) {
-        if (!customer.isLoggedIn()){
+        if (!customer.isLoggedIn()) {
             return new ErrorMessage((short) 7);
         }
-        Set<String> registerNamesSet= dataBase.getUserNameToCustomer().keySet();
-        Integer numOfRegisters= registerNamesSet.size(); //num of registersUsers
-        String registerListString= numOfRegisters.toString();//String that will be send as optional bytes array
-        for (String name: registerNamesSet){
-            registerListString+= name+"\0";  //filling the string with the names of the register users
+        Set<String> registerNamesSet = dataBase.getUserNameToCustomer().keySet();
+        Integer numOfRegisters = registerNamesSet.size(); //num of registersUsers
+        String registerListString = numOfRegisters.toString();//String that will be send as optional bytes array
+        for (String name : registerNamesSet) {
+            registerListString += name + "\0";  //filling the string with the names of the register users
         }
-        return new AckMessage((short)7,registerListString.getBytes()); //string converted to array bytes
+        return new AckMessage((short) 7, registerListString.getBytes()); //string converted to array bytes
     }
 }
