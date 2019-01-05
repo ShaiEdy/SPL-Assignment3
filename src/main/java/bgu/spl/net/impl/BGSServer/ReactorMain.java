616a4c11740c74de60f9051b36e8dcdf271481bc
עדי <agotliber@gmail..com>
2019-01-05 22:24:17 +0200

    yes

--- src/main/java/bgu/spl/net/impl/BGSServer/ReactorMain.java
+++ src/main/java/bgu/spl/net/impl/BGSServer/ReactorMain.java
@@ -1,15 +1,41 @@
 package bgu.spl.net.impl.BGSServer;
 
+import bgu.spl.net.api.DataBase;
+import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.api.bidi.BidiMessagingProtocol;
+import bgu.spl.net.api.bidi.BidiMessagingProtocolImpl;
+import bgu.spl.net.api.bidi.MessageEncoderDecoderImp;
+import bgu.spl.net.impl.messages.Message;
+import bgu.spl.net.impl.rci.ObjectEncoderDecoder;
+import bgu.spl.net.impl.rci.RemoteCommandInvocationProtocol;
 import bgu.spl.net.srv.Server;
 
+import java.util.function.Supplier;
+
 public class ReactorMain {
-    /*
-    DataBase dataBase = new DataBase();
-     Server.reactor(
-             args[1],
-                args[0], //port
-                        () ->  new RemoteCommandInvocationProtocol<>(feed), //protocol factory
-    ObjectEncoderDecoder::new //message encoder decoder factory
-            ).serve();
-            */
+    public static void main(String[] args) {
+
+        DataBase dataBase = new DataBase();
+        Server reactor = Server.reactor(3,
+                7000,
+                () -> {
+                    BidiMessagingProtocol<Message> bidiMessagingProtocol = new BidiMessagingProtocolImpl(dataBase);
+                    return bidiMessagingProtocol;
+                },
+                ()->{
+                    MessageEncoderDecoder<Message> encoderDecoder = new MessageEncoderDecoderImp();
+                    return encoderDecoder;
+                });
+        reactor.serve();
+
+        /*
+        Server.reactor(
+                3,
+                7000, //port
+                () -> BidiMessagingProtocol<Message>BidiMessagingProtocol(), //protocol factory
+                ObjectEncoderDecoder::new //message encoder decoder factory
+        ).serve();
+        */
+    }
+
 }
--- src/main/java/bgu/spl/net/srv/NonBlockingConnectionHandler.java
+++ src/main/java/bgu/spl/net/srv/NonBlockingConnectionHandler.java
@@ -51,12 +51,7 @@ public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
                     while (buf.hasRemaining()) {
                         T nextMessage = encdec.decodeNextByte(buf.get());
                         if (nextMessage != null) {
-                            //T response =
                             protocol.process(nextMessage);
-                           // if (response != null) {
-                             //   writeQueue.add(ByteBuffer.wrap(encdec.encode(response)));
-                               // reactor.updateInterestedOps(chan, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-                          //  }
                         }
                     }
                 } finally {
@@ -121,6 +116,7 @@ public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
 
     @Override
     public void send(T msg) {
-
+        writeQueue.add(ByteBuffer.wrap(encdec.encode(msg)));
+        reactor.updateInterestedOps(chan, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
     }
 }
