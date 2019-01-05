53322a577450d6ff4dae5e52fe035955bcf517e5
Shai <shaiadi1@gmail.com>
2019-01-05 13:11:43 +0200

    Hey

--- src/main/java/bgu/spl/net/impl/messages/UserListMessage.java
+++ src/main/java/bgu/spl/net/impl/messages/UserListMessage.java
@@ -2,7 +2,11 @@ package bgu.spl.net.impl.messages;
 
 import bgu.spl.net.api.Customer;
 import bgu.spl.net.api.DataBase;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import java.util.Vector;
 
 public class UserListMessage extends Message {
     private short opcode= 7;
@@ -17,14 +21,39 @@ public class UserListMessage extends Message {
         if (!customer.isLoggedIn()) {
             return new ErrorMessage((short) 7);
         }
+        //byte[] ackMessageOptionalArr = new byte[0];
         Set<String> registerNamesSet = dataBase.getUserNameToCustomer().keySet();
         byte[] numOfRegistersBytesArr = shortToBytes((short) registerNamesSet.size());//num of registersUsers
-        String registerListString = "";//String that will be send as optional bytes array
-        for (String name : registerNamesSet) {
-            registerListString += name + "\0";  //filling the string with the names of the register users
-        }
-        byte[] ackMessageOptionalArr = merge2Arrays(numOfRegistersBytesArr, registerListString.getBytes());
+        List<Byte> bytesToReturn = new Vector<>(convertbytesArrayToBytesList(numOfRegistersBytesArr));
+        //ackMessageOptionalArr = merge2Arrays(ackMessageOptionalArr,numOfRegistersBytesArr);
 
-        return new AckMessage((short) 7, ackMessageOptionalArr); //string converted to array bytes
+        for (String name : registerNamesSet) {
+            bytesToReturn.addAll(convertbytesArrayToBytesList(name.getBytes()));// add the notification type
+            bytesToReturn.add((byte) '\0');
+        }
+        //byte[] ackMessageOptionalArr = merge2Arrays(numOfRegistersBytesArr, registerListString.getBytes());
+
+        byte[] ArrayToReturn = convertListToArray(bytesToReturn); //convert to byte[]
+
+        return new AckMessage((short) 7, ArrayToReturn); //string converted to array bytes
+    }
+    private byte[] convertListToArray(List<Byte> bytes){
+        byte[] bytestoreturn= new byte[bytes.size()];
+        int index=0;
+        for (Byte Byte: bytes){
+            bytestoreturn[index]= Byte;
+            index++;
+        }
+        return bytestoreturn;
+    }
+
+    private List<Byte> convertbytesArrayToBytesList(byte[] bytes){
+        Byte[] Bytes = new Byte[bytes.length];
+        int index=0;
+        for(byte b: bytes) {
+            Bytes[index] = b;
+            index++;
+        }
+        return Arrays.asList(Bytes);
     }
 }
