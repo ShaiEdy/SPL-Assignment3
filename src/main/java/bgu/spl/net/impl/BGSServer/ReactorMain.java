23de7750d9c0241a5e0282c7b468e26cb0aa0c0d
עדי <agotliber@gmail..com>
2019-01-06 15:07:00 +0200

    yes

--- src/main/java/bgu/spl/net/impl/BGSServer/ReactorMain.java
+++ src/main/java/bgu/spl/net/impl/BGSServer/ReactorMain.java
@@ -6,18 +6,15 @@ import bgu.spl.net.api.bidi.BidiMessagingProtocol;
 import bgu.spl.net.api.bidi.BidiMessagingProtocolImpl;
 import bgu.spl.net.api.bidi.MessageEncoderDecoderImp;
 import bgu.spl.net.impl.messages.Message;
-import bgu.spl.net.impl.rci.ObjectEncoderDecoder;
-import bgu.spl.net.impl.rci.RemoteCommandInvocationProtocol;
 import bgu.spl.net.srv.Server;
 
-import java.util.function.Supplier;
 
 public class ReactorMain {
     public static void main(String[] args) {
 
         DataBase dataBase = new DataBase();
-        Server reactor = Server.reactor(3,
-                7002,
+        Server reactor = Server.reactor(Integer.parseInt(args[1]),
+                Integer.parseInt(args[0]),
                 () -> {
                     BidiMessagingProtocol<Message> bidiMessagingProtocol = new BidiMessagingProtocolImpl(dataBase);
                     return bidiMessagingProtocol;
